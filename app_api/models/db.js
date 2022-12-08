28be305f65a6847e4194cd14392f1cac29b2795c
Alpay Özer <alpayozer7@gmail.com>
2022-12-08 10:29:22 +0300

    ders11son

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-//var dbURI = 'mongodb://localhost/mekanbul';
-var dbURI = "mongodb+srv://alpay:1234@mekanbul.o2z26f1.mongodb.net/mekanbul?retryWrites=true&w=majority";
+var dbURI = 'mongodb://localhost/mekanbul';
+//var dbURI = "mongodb+srv://alpay:1234@mekanbul.o2z26f1.mongodb.net/mekanbul?retryWrites=true&w=majority";
 require("./mekansema"); //db.js dosyası mekansema içindeki her şeyi kullanabilmesi için tanıttık
 
 
